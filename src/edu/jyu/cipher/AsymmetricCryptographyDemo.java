71a5069ac0f71b17202af4e5465d4833bec964ca
Jason <1031500191@qq.com>
2017-09-03 22:59:54 +0800

    分别演示公钥加密私钥解密和私钥加密公钥解密

--- src/edu/jyu/cipher/AsymmetricCryptographyDemo.java
+++ src/edu/jyu/cipher/AsymmetricCryptographyDemo.java
@@ -19,8 +19,13 @@ import edu.jyu.util.Tools;
  *
  */
 public class AsymmetricCryptographyDemo {
+	/**
+	 * 用私钥加密
+	 * 
+	 * @throws Exception
+	 */
 	@Test
-	public void encrypt() throws Exception {
+	public void encryptByPrivateKey() throws Exception {
 		Cipher cipher = Cipher.getInstance("RSA");
 		// 获得秘钥对
 		KeyPair keyPair = KeyPairGenerator.getInstance("RSA").generateKeyPair();
@@ -34,11 +39,52 @@ public class AsymmetricCryptographyDemo {
 		Tools.saveData(result, "hjj.data");
 	}
 
+	/**
+	 * 用公钥解密
+	 * 
+	 * @throws Exception
+	 */
 	@Test
-	public void decrypt() throws Exception {
+	public void decryptByPublicKey() throws Exception {
 		Cipher cipher = Cipher.getInstance("RSA");
 		Key publicKey = Tools.readKey("hjj_public.key");
 		cipher.init(Cipher.DECRYPT_MODE, publicKey);
+		byte[] data = Tools.readData("hjj.data");
+		byte[] result = cipher.doFinal(data);
+		System.out.println(new String(result));
+	}
+	
+	//--------------------------------------------
+	/**
+	 * 用公钥加密
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void encryptByPublicKey() throws Exception {
+		Cipher cipher = Cipher.getInstance("RSA");
+		// 获得秘钥对
+		KeyPair keyPair = KeyPairGenerator.getInstance("RSA").generateKeyPair();
+		// 私钥
+		PrivateKey privateKey = keyPair.getPrivate();
+		// 公钥
+		PublicKey publicKey = keyPair.getPublic();
+		Tools.saveKey(privateKey, "hjj_private.key");
+		cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+		byte[] result = cipher.doFinal("嘉应学院".getBytes());
+		Tools.saveData(result, "hjj.data");
+	}
+
+	/**
+	 * 用私钥解密
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void decryptByPrivateKey() throws Exception {
+		Cipher cipher = Cipher.getInstance("RSA");
+		Key privateKey = Tools.readKey("hjj_private.key");
+		cipher.init(Cipher.DECRYPT_MODE, privateKey);
 		byte[] data = Tools.readData("hjj.data");
 		byte[] result = cipher.doFinal(data);
 		System.out.println(new String(result));
